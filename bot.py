472879a6efc8e42b91305209c6516dfaa1dcf50a
alonrabinowitz <alon.rabinowitz@gmail.com>
2022-05-11 12:56:28 -0700

    .

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 intents = discord.Intents.all()
 
-client = commands.Bot(command_prefix = '>', intents = intents)
+client = commands.Bot(command_prefix = '.', intents = intents)
 
 
 
